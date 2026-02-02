27673b994b2d3481a63ed5860a838cd1affec1d9
bushrabilan <bushrabilan9672@gmail.com>
2026-02-02 14:43:01 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form')
+  const taskList = document.querySelector('#tasks')
+  const input = document.querySelector('#new-task-description')
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const text = input.value.trim()
+    if (text === '') return
+    const li = document.createElement('li')
+    li.textContent = text
+    taskList.appendChild(li)
+    input.value = ''
+  })
 });
